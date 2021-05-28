02b8f6b788a975f50acc8d787937952ff8c6b448
Marylou5 <80683811+Marylou5@users.noreply.github.com>
2021-05-28 09:51:15 +0200

    Add files via upload

---
+++ memo/memo.js
@@ -0,0 +1,62 @@
+var modal = document.getElementsByClassName('modal');
+
+// Get the button that opens the modal
+var btn = document.getElementsByClassName("myBtn");
+
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close");
+
+// When the user clicks the button, open the modal 
+btn[0].onclick = function() {
+    modal[0].style.display = "block";
+}
+
+btn[1].onclick = function() {
+    modal[1].style.display = "block";
+}
+// When the user clicks on <span> (x), close the modal
+span[0].onclick = function() {
+    modal[0].style.display = "none";
+}
+
+span[1].onclick = function() {
+    modal[1].style.display = "none";
+}
+// utilisateur peut cliquer partout autre que dans le modal pour le fermer
+window.onclick = function(event) {
+    if (event.target == modal[0]) {
+         modal[0].style.display = "none";
+     }
+    if (event.target == modal[1]) {
+         modal[1].style.display = "none";
+     }  
+}
+
+
+
+// Get the modal
+//var modal = document.getElementById("1Modal");
+
+// Get the button that opens the modal
+//var btn = document.getElementById("btn");
+
+// Get the <span> element that closes the modal
+//var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+//btn.onclick = function() {
+//  modal.style.display = "block";
+//}
+
+// When the user clicks on <span> (x), close the modal
+//span.onclick = function() {
+//  modal.style.display = "none";
+//}
+
+// When the user clicks anywhere outside of the modal, close it
+//window.onclick = function(event) {
+  //if (event.target == modal) {
+    //modal.style.display = "none";
+ // }
+//}
